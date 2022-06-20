34b9485bb6b91507a37140092271732186dc1c45
Jefrin Jojan <jefrin@fb.com>
2022-06-20 15:26:31 -0700

    2nd commit - working Splash

--- app/src/main/java/com/example/harvestfresh/SplashScreenActivity.java
+++ app/src/main/java/com/example/harvestfresh/SplashScreenActivity.java
@@ -10,7 +10,7 @@ public class SplashScreenActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        //handle signup
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
