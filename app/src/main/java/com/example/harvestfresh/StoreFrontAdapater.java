0739d38e7b3fd7fa7c6e42d060a93bf795141c81
jjojan <68476473+jjojan@users.noreply.github.com>
2022-06-23 10:52:21 -0700

    Added StoreFrontAdapater file to bind Store Name and Image to Recycler View, added glide dependencies to load images

---
+++ app/src/main/java/com/example/harvestfresh/StoreFrontAdapater.java
@@ -0,0 +1,76 @@
+package com.example.harvestfresh;
+
+import android.content.Context;
+import android.media.Image;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.parse.ParseFile;
+
+import java.util.List;
+
+public class StoreFrontAdapater extends RecyclerView.Adapter<StoreFrontAdapater.ViewHolder> {
+
+    private Context context;
+    private List<StoreFront> stores;
+
+    public StoreFrontAdapater(Context context, List<StoreFront> stores) {
+        this.context = context;
+        this.stores = stores;
+    }
+
+    @NonNull
+    @Override
+    public StoreFrontAdapater.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.item_store, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull StoreFrontAdapater.ViewHolder holder, int position) {
+        StoreFront store = stores.get(position);
+        holder.bind(store);
+    }
+
+    @Override
+    public int getItemCount() {
+        return stores.size();
+    }
+
+    public void clear() {
+        stores.clear();
+        notifyDataSetChanged();
+    }
+
+    public void addAll(List<StoreFront> list) {
+        stores.addAll(list);
+        notifyDataSetChanged();
+    }
+
+    class ViewHolder extends RecyclerView.ViewHolder {
+
+        private TextView tvStoreName;
+        private ImageView ivImage;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            tvStoreName = itemView.findViewById(R.id.tvName);
+            ivImage = itemView.findViewById(R.id.ivStore);
+        }
+
+        public void bind(StoreFront store){
+            tvStoreName.setText(store.getName());
+            ParseFile image = store.getImage();
+            if(image != null) {
+                Glide.with(context).load(image.getUrl()).into(ivImage);
+            }
+        }
+    }
+}
