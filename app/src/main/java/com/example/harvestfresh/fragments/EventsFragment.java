36a41c1da2771a219a4ff31561bd813710cb2d0a
jjojan <68476473+jjojan@users.noreply.github.com>
2022-06-24 12:00:23 -0700

    Created new SearchFragment

--- app/src/main/java/com/example/harvestfresh/fragments/EventsFragment.java
+++ app/src/main/java/com/example/harvestfresh/fragments/EventsFragment.java
@@ -103,168 +103,167 @@ public class EventsFragment extends Fragment implements OnMapReadyCallback {
     @Override
     public void onMapReady(GoogleMap googleMap) {
         map = googleMap;
-        getLocation();
-//        googleMap.addMarker(new MarkerOptions()
-//                .position(new LatLng(51.5072, 0.1276))
-//                .title("London"));
-//        LatLng latLng = new LatLng(51.5072, 0.1276);
-//        map.moveCamera(CameraUpdateFactory.newLatLng(latLng));
-//        map.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng,15f));
+        googleMap.addMarker(new MarkerOptions()
+                .position(new LatLng(51.5072, 0.1276))
+                .title("London"));
+        LatLng latLng = new LatLng(51.5072, 0.1276);
+        map.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+        map.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng,15f));
     }
 
-    private void getLocation() {
-        progressDialog.show();
-        if(Build.VERSION.SDK_INT>=23){
-            if(checkPermission()){
-                getDeviceLocation();
-            }
-            else{
-                requestPermission();
-            }
-        }
-        else{
-            getDeviceLocation();
-        }
-    }
-
-    private void requestPermission() {
-        ActivityCompat.requestPermissions(getActivity(),permissions_all,PERMISSION_CODE);
-    }
-
-    private boolean checkPermission() {
-        for(int i=0;i<permissions_all.length;i++){
-            int result= ContextCompat.checkSelfPermission(getContext(),permissions_all[i]);
-            if(result== PackageManager.PERMISSION_GRANTED){
-                continue;
-            }
-            else {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    private void getDeviceLocation() {
-        locationManager=(LocationManager) getActivity().getSystemService(Service.LOCATION_SERVICE);
-        isGpsLocation=locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
-        isNetworklocation=locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
-        if(!isGpsLocation && !isNetworklocation){
-            showSettingForLocation();
-            getLastlocation();
-        }
-        else{
-            getFinalLocation();
-        }
-    }
-
-    private void getLastlocation() {
-        if(locationManager!=null) {
-            try {
-                Criteria criteria = new Criteria();
-                String provider = locationManager.getBestProvider(criteria,false);
-                Location location=locationManager.getLastKnownLocation(provider);
-            } catch (SecurityException e) {
-                e.printStackTrace();
-            }
-        }
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        switch (requestCode){
-            case PERMISSION_CODE:
-                if(grantResults.length>0 && grantResults[0]== PackageManager.PERMISSION_GRANTED){
-                    getFinalLocation();
-                }
-                else{
-                    Toast.makeText(getContext(), "Permission Failed", Toast.LENGTH_SHORT).show();
-                }
-        }
-    }
-
-    private void getFinalLocation() {
-        //one thing i missed in permission let's complete it
-        try{
-            if(isGpsLocation){
-                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,1000*60*1,10, (LocationListener) getContext());
-                if(locationManager!=null){
-                    loc=locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-                    if(loc!=null){
-                        updateUi(loc);
-                    }
-                }
-            }
-            else if(isNetworklocation){
-                locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER,1000*60*1,10, (LocationListener) getContext());
-                if(locationManager!=null){
-                    loc=locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
-                    if(loc!=null){
-                        updateUi(loc);
-                    }
-                }
-            }
-            else{
-                Toast.makeText(getContext(), "Can't Get Location", Toast.LENGTH_SHORT).show();
-            }
-
-        }catch (SecurityException e){
-            Toast.makeText(getContext(), "Can't Get Location", Toast.LENGTH_SHORT).show();
-        }
-
-    }
-
-    private void updateUi(Location loc) {
-        if(loc.getLatitude()==0 && loc.getLongitude()==0){
-            getDeviceLocation();
-        }
-        else{
-            progressDialog.dismiss();
-            LatLng latLng=new LatLng(loc.getLatitude(),loc.getLongitude());
-            map.addMarker(new MarkerOptions().position(latLng).title("Current Location"));
-            map.moveCamera(CameraUpdateFactory.newLatLng(latLng));
-            map.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng,15f));
-
-        }
-
-    }
-
-    private void showSettingForLocation() {
-        AlertDialog.Builder al=new AlertDialog.Builder(getContext());
-        al.setTitle("Location Not Enabled!");
-        al.setMessage("Enable Location ?");
-        al.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                Intent intent=new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
-                startActivity(intent);
-            }
-        });
-        al.setNegativeButton("No", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                dialog.cancel();
-            }
-        });
-        al.show();
-    }
-
-
-    public void onLocationChanged(Location location) {
-        updateUi(location);
-    }
-
-    public void onStatusChanged(String provider, int status, Bundle extras) {
-
-    }
-
-
-    public void onProviderEnabled(String provider) {
-
-    }
-
-    public void onProviderDisabled(String provider) {
-
-    }
+//    private void getLocation() {
+//        progressDialog.show();
+//        if(Build.VERSION.SDK_INT>=23){
+//            if(checkPermission()){
+//                getDeviceLocation();
+//            }
+//            else{
+//                requestPermission();
+//            }
+//        }
+//        else{
+//            getDeviceLocation();
+//        }
+//    }
+//
+//    private void requestPermission() {
+//        ActivityCompat.requestPermissions(getActivity(),permissions_all,PERMISSION_CODE);
+//    }
+//
+//    private boolean checkPermission() {
+//        for(int i=0;i<permissions_all.length;i++){
+//            int result= ContextCompat.checkSelfPermission(getContext(),permissions_all[i]);
+//            if(result== PackageManager.PERMISSION_GRANTED){
+//                continue;
+//            }
+//            else {
+//                return false;
+//            }
+//        }
+//        return true;
+//    }
+//
+//    private void getDeviceLocation() {
+//        locationManager=(LocationManager) getActivity().getSystemService(Service.LOCATION_SERVICE);
+//        isGpsLocation=locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+//        isNetworklocation=locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+//        if(!isGpsLocation && !isNetworklocation){
+//            showSettingForLocation();
+//            getLastlocation();
+//        }
+//        else{
+//            getFinalLocation();
+//        }
+//    }
+//
+//    private void getLastlocation() {
+//        if(locationManager!=null) {
+//            try {
+//                Criteria criteria = new Criteria();
+//                String provider = locationManager.getBestProvider(criteria,false);
+//                Location location=locationManager.getLastKnownLocation(provider);
+//            } catch (SecurityException e) {
+//                e.printStackTrace();
+//            }
+//        }
+//    }
+//
+//    @Override
+//    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+//        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+//        switch (requestCode){
+//            case PERMISSION_CODE:
+//                if(grantResults.length>0 && grantResults[0]== PackageManager.PERMISSION_GRANTED){
+//                    getFinalLocation();
+//                }
+//                else{
+//                    Toast.makeText(getContext(), "Permission Failed", Toast.LENGTH_SHORT).show();
+//                }
+//        }
+//    }
+//
+//    private void getFinalLocation() {
+//        //one thing i missed in permission let's complete it
+//        try{
+//            if(isGpsLocation){
+//                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,1000*60*1,10, (LocationListener) getContext());
+//                if(locationManager!=null){
+//                    loc=locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+//                    if(loc!=null){
+//                        updateUi(loc);
+//                    }
+//                }
+//            }
+//            else if(isNetworklocation){
+//                locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER,1000*60*1,10, (LocationListener) getContext());
+//                if(locationManager!=null){
+//                    loc=locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+//                    if(loc!=null){
+//                        updateUi(loc);
+//                    }
+//                }
+//            }
+//            else{
+//                Toast.makeText(getContext(), "Can't Get Location", Toast.LENGTH_SHORT).show();
+//            }
+//
+//        }catch (SecurityException e){
+//            Toast.makeText(getContext(), "Can't Get Location", Toast.LENGTH_SHORT).show();
+//        }
+//
+//    }
+//
+//    private void updateUi(Location loc) {
+//        if(loc.getLatitude()==0 && loc.getLongitude()==0){
+//            getDeviceLocation();
+//        }
+//        else{
+//            progressDialog.dismiss();
+//            LatLng latLng=new LatLng(loc.getLatitude(),loc.getLongitude());
+//            map.addMarker(new MarkerOptions().position(latLng).title("Current Location"));
+//            map.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+//            map.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng,15f));
+//
+//        }
+//
+//    }
+//
+//    private void showSettingForLocation() {
+//        AlertDialog.Builder al=new AlertDialog.Builder(getContext());
+//        al.setTitle("Location Not Enabled!");
+//        al.setMessage("Enable Location ?");
+//        al.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+//            @Override
+//            public void onClick(DialogInterface dialog, int which) {
+//                Intent intent=new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+//                startActivity(intent);
+//            }
+//        });
+//        al.setNegativeButton("No", new DialogInterface.OnClickListener() {
+//            @Override
+//            public void onClick(DialogInterface dialog, int which) {
+//                dialog.cancel();
+//            }
+//        });
+//        al.show();
+//    }
+//
+//
+//    public void onLocationChanged(Location location) {
+//        updateUi(location);
+//    }
+//
+//    public void onStatusChanged(String provider, int status, Bundle extras) {
+//
+//    }
+//
+//
+//    public void onProviderEnabled(String provider) {
+//
+//    }
+//
+//    public void onProviderDisabled(String provider) {
+//
+//    }
 
 }
