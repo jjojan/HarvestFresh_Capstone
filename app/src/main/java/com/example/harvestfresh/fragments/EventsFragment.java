ff5a745c20882ec2207a7c404157025549ed35b2
jjojan <68476473+jjojan@users.noreply.github.com>
2022-06-28 10:02:25 -0700

    Added custom icon to Maps markers
    https://imgur.com/a/oNvOobn

--- app/src/main/java/com/example/harvestfresh/fragments/EventsFragment.java
+++ app/src/main/java/com/example/harvestfresh/fragments/EventsFragment.java
@@ -1,55 +1,33 @@
 package com.example.harvestfresh.fragments;
 
-import static android.content.ContentValues.TAG;
-
 import android.Manifest;
-import android.app.Dialog;
-import android.app.ProgressDialog;
-import android.app.Service;
-import android.content.DialogInterface;
-import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.location.Criteria;
 import android.location.Location;
-import android.location.LocationManager;
-import android.location.LocationRequest;
-import android.os.Build;
 import android.os.Bundle;
-import android.location.LocationListener;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.appcompat.app.AlertDialog;
 import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
-import android.os.Looper;
-import android.provider.Settings;
-import android.text.TextUtils;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
 
+import com.bumptech.glide.load.engine.Resource;
 import com.example.harvestfresh.R;
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.ErrorDialogFragment;
-import com.google.android.gms.common.GooglePlayServicesUtil;
 import com.google.android.gms.common.api.Status;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.location.LocationSettingsRequest;
-import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.libraries.places.api.Places;
 import com.google.android.libraries.places.api.model.Place;
@@ -90,6 +68,7 @@ public class EventsFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
         fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(getActivity());
         fetchLastLocation();
         SupportMapFragment mapFragment =
@@ -130,7 +109,7 @@ public class EventsFragment extends Fragment {
 
     private void placeMarkers() {
         LatLng newLang = new LatLng(37.474300, -122.139038);
-        MarkerOptions newMarker = new MarkerOptions().position(newLang).title("Marker 1");
+        MarkerOptions newMarker = new MarkerOptions().position(newLang).title("Marker 1").icon(BitmapDescriptorFactory.fromResource(R.drawable.mapsicon));
         markerMap.addMarker(newMarker);
     }
 
@@ -155,7 +134,7 @@ public class EventsFragment extends Fragment {
 
     private void updateCurrentLocation() {
         LatLng latLng = new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude());
-        MarkerOptions markerOptions = new MarkerOptions().position(latLng).title("Current Location");
+        MarkerOptions markerOptions = new MarkerOptions().position(latLng).title("Current Location").icon(BitmapDescriptorFactory.fromResource(R.drawable.mapsicon));
         mMap.animateCamera(CameraUpdateFactory.newLatLng(latLng));
         mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 12));
         mMap.addMarker(markerOptions);
