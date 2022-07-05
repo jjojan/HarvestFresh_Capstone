63af9c193e4ce4c09e93d205f23efd58e3e7ae41
jjojan <68476473+jjojan@users.noreply.github.com>
2022-07-05 11:59:25 -0700

    Added Recycler view to show list of products on the detail view for each store
    https://imgur.com/a/NggeVES

--- app/src/main/java/com/example/harvestfresh/DetailActivity.java
+++ app/src/main/java/com/example/harvestfresh/DetailActivity.java
@@ -2,15 +2,80 @@ package com.example.harvestfresh;
 
 import android.os.Bundle;
 import android.os.PersistableBundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseFile;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
+
+import org.parceler.Parcels;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 public class DetailActivity extends AppCompatActivity {
+
+    private static int PRODUCT_LIMIT = 20;
+
+    private RecyclerView rvProduct;
+    private ProductListingAdapter fragmentAdapter;
+    private List<ProductListing> allProducts;
+
+    StoreFront store;
+    TextView tvStoreName;
+    ImageView ivStoreImage;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+
+        tvStoreName = (TextView) findViewById(R.id.tvStoreName);
+        ivStoreImage = (ImageView) findViewById(R.id.ivStoreImage);
+
+        store = (StoreFront) Parcels.unwrap(getIntent().getParcelableExtra(StoreFront.class.getSimpleName()));
+
+        tvStoreName.setText(store.getName());
+
+        ParseFile photo = store.getImage();
+        if (photo != null) {
+            Glide.with(this).load(photo.getUrl()).into(ivStoreImage);
+        }
+
+        rvProduct = findViewById(R.id.rvProducts);
+        rvProduct.setLayoutManager(new LinearLayoutManager(this));
+
+        allProducts = new ArrayList<>();
+        fragmentAdapter = new ProductListingAdapter(this, allProducts);
+        rvProduct.setAdapter(fragmentAdapter);
+
+        queryProducts();
+    }
+
+    private void queryProducts() {
+        ParseQuery<ProductListing> query = ParseQuery.getQuery(ProductListing.class);
+        query.setLimit(PRODUCT_LIMIT);
+        query.whereEqualTo(ProductListing.KEY_USER, store.getUser());
+
+        query.findInBackground(new FindCallback<ProductListing>() {
+            @Override
+            public void done(List<ProductListing> products, ParseException e) {
+                if (e != null) {
+                    return;
+                }
+                allProducts.addAll(products);
+                fragmentAdapter.notifyDataSetChanged();
+            }
+        });
     }
 }
