c44886d76e1ef32325d04d49d62a39c6c1d93a21
jjojan <68476473+jjojan@users.noreply.github.com>
2022-07-06 14:11:21 -0700

    Added function for adding items to cart

--- app/src/main/java/com/example/harvestfresh/StoreFrontAdapter.java
+++ app/src/main/java/com/example/harvestfresh/StoreFrontAdapter.java
@@ -2,7 +2,6 @@ package com.example.harvestfresh;
 
 import android.content.Context;
 import android.content.Intent;
-import android.media.Image;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
